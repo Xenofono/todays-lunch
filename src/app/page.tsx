57dcbc0788286dfcf0431e0bb60b18b3249d8e2a
kristoffer <kristoffer.nasstrom@gmail.com>
2025-06-09 18:07:09 +0200

    added time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,8 +21,9 @@ export default async function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
 
-            <div className="text-center">
-                <h1 className="text-3xl font-bold mb-2">Restaurant Menus</h1>
+            <div>
+                <h1 className="text-3xl font-bold">Restaurant Menus</h1>
+                <p className="text-lg">Calculated at {new Date().toLocaleString('sv-SE', {timeZone: 'Europe/Stockholm'})} Swedish time</p>
             </div>
 
             <RestaurantGrid restaurants={restaurants} />
